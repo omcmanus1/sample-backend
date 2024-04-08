2afc2c3106d2ceece648e1422f6c1ee36c7c6ca2
omcmanus1 <omcmanus1@gmail.com>
2024-04-08 23:19:03 +0100

    update accounts processing

--- src/mongodb/models/Account.ts
+++ src/mongodb/models/Account.ts
@@ -7,7 +7,8 @@ const accountSchema = new Schema(
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    status: { type: String },
+    status: { type: String, default: "active" },
+    deletedAt: { type: Date },
   },
   { timestamps: true }
 );
